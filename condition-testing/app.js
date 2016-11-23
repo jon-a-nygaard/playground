a42ca2a5729494f62025b1779ce147dd66987cb1
jon-a-nygaard <jon.arild@highsoft.com>
2016-11-23 22:05:03 +0100

    Added form and inputs to layout, and started modifying application accordingly.

--- condition-testing/app.js
+++ condition-testing/app.js
@@ -1,29 +1,36 @@
+(function () {
+var button = document.getElementById('submit'),
+    table = document.getElementById('result');
 var combinations = function(n) {
-    var r = [];
-    for (var i = 0; i < (1 << n); i++) {
-      var c = [];
-      for (var j = 0; j < n; j++) {
-        c.push(i & (1 << j) ? true : false);
-      }
-      r.push(c);
+  var r = [];
+  for (var i = 0; i < (1 << n); i++) {
+    var c = [];
+    for (var j = 0; j < n; j++) {
+      c.push(i & (1 << j) ? true : false);
     }
-    return r;
-  },
-  test1 = function(isNull, stacking, connectNulls) {
-    return !(isNull && !stacking && connectNulls);
-  },
-  test2 = function(isNull, stacking, connectNulls) {
-    return !isNull && stacking && !connectNulls;
-  },
-  table = document.getElementById('result'),
+    r.push(c);
+  }
+  return r;
+};
+var test1 = function(isNull, stacking, connectNulls) {
+  return !(isNull && !stacking && connectNulls);
+};
+var test2 = function(isNull, stacking, connectNulls) {
+  return !isNull && stacking && !connectNulls;
+};
+var runCombinations = function () {
   combos = combinations(3);
-combos.forEach(function(combo) {
-	var row = table.insertRow();
-  var values = [combo[0], combo[1], combo[2], test1.apply(this, combo), test2.apply(this, combo)];
-  values.forEach(function (v) {
-  	var cell = row.insertCell();
-    var content = document.createTextNode('' + v)
-    cell.appendChild(content);
-    cell.className = (v ? 'bg-success' : 'bg-danger')
+  combos.forEach(function(combo) {
+    var row = table.insertRow();
+    var values = [combo[0], combo[1], combo[2], test1.apply(this, combo), test2.apply(this, combo)];
+    values.forEach(function (v) {
+      var cell = row.insertCell();
+      var content = document.createTextNode('' + v)
+      cell.appendChild(content);
+      cell.className = (v ? 'bg-success' : 'bg-danger')
+    })
   })
-})
+};
+
+button.onclick = runCombinations;
+}());
